95fd522c6ed33988e97bdf92829918a888ae31a7
sefatanam <sefatanam@gmail.com>
2020-12-17 20:11:29 +0600

    email retrieved

--- src/app/inbox/services/email.service.ts
+++ src/app/inbox/services/email.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+interface IEmailSummary {
+  id: string;
+  subject: string;
+  from: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
+  rootUrl = 'https://api.angular-email.com';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getEmails() {
+    return this.http.get<IEmailSummary[]>(`${this.rootUrl}/emails`); // for our interceptor by default its with credentials is true
+  }
 }
