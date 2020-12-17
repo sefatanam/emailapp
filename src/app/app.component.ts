ae8266fb12ae920fd2e9f3a969cae84e5b53a232
sefatanam <sefatanam@gmail.com>
2020-12-17 16:31:07 +0600

    signup observable updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,18 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {AuthService} from "./auth/services/auth.service";
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  isSignedIn: boolean;
+  isSignedIn$: BehaviorSubject<boolean>;
 
   constructor(private authService: AuthService) {
+    this.isSignedIn$ = this.authService.isSignedIn$;
   }
 
-  ngOnInit(): void {
-    this.authService.isSignedIn$.subscribe((signedInStatus) => {
-      this.isSignedIn = signedInStatus;
-    });
-  }
 }
