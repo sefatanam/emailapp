432096d4d93c73e84edaa32bb547613f2f3c758b
sefatanam <sefatanam@gmail.com>
2020-12-17 19:36:22 +0600

    inbox modules files initiate

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { InboxRoutingModule } from './inbox-routing.module';
 import { HomeComponent } from './home/home.component';
+import { CreateComponent } from './create/create.component';
+import { IndexComponent } from './index/index.component';
+import { ReplyComponent } from './reply/reply.component';
+import { ShowComponent } from './show/show.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, CreateComponent, IndexComponent, ReplyComponent, ShowComponent],
   imports: [
     CommonModule,
     InboxRoutingModule
